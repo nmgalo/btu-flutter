c68a99d4df73eae3b30ed5c4220934e9547f1083
Nika <nikolozamgalo@gmail.com>
2021-04-05 14:17:48 +0400

    add assignment 2

---
+++ assignment_2/lib/main.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  int userTotalScore = 0;
+  int botTotalScore = 0;
+
+  int userScore = 0;
+  int botScore = 0;
+
+  while (true) {
+    stdout.write("type EXIT to exit, press any key to continue: ");
+    // String userChoice = stdin.readLineSync()?.toLowerCase();
+    if (stdin.readLineSync()?.toLowerCase() == "exit") break;
+
+    userScore = roll() + roll();
+    botScore = roll() + roll();
+    userTotalScore += userScore;
+    botTotalScore += botScore;
+    print("user sum $userScore");
+    print("bot sum $botScore");
+  }
+
+  if (botTotalScore > userTotalScore) {
+    print("Winner: BOT");
+  } else if (botTotalScore < userTotalScore) {
+    print("You are winner <333");
+  } else {
+    print("Game ended in a draw");
+  }
+}
+
+int roll() => Random().nextInt(7);
